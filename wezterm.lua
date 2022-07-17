b4346985c9cc8ca30076ddec2e7368eb6f50b2eb
Manh Luong <luongquangmanh85@gmail.com>
2022-07-17 19:59:27 +0700

    Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require "wezterm";
+
+return {
+    check_for_updates = false,
+    cursor_blink_rate = 500,
+    color_scheme = "Gruvbox Dark",
+    font = wezterm.font("Victor Mono", { weight = "Medium" }),
+    font_size = 11,
+    initial_cols = 90,
+    initial_rows = 24,
+    line_height = 0.85,
+    show_tab_index_in_tab_bar = false,
+    use_fancy_tab_bar = true,
+    use_resize_increments = true,
+
+    window_frame = {
+        font = wezterm.font("Inter", { weight = "Bold" }),
+        font_size = 11,
+    },
+}
